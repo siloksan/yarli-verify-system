b3ad7291e9cf9981d3e47236a265eb9db7235fbd
siloksan <107646198+siloksan@users.noreply.github.com>
2026-02-12 11:18:28 +0000

    mobile|chore: create base sctructure of project folders

---
+++ apps/scanner/app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Text>Сканнер включается только при переходе с веб клиента</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
